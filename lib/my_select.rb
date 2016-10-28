6b76d5dee90fb0fdd34de2013842e23914b699cb
Jennifer Wong <wongjenn@yahoo.com>
2016-10-28 23:10:05 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+  new_array = []
+  i = 0
+
+  if collection.empty?
+    return []
+  else
+    while i < collection.length
+
+      if yield(collection[i]) == true
+        new_array.push(collection[i])
+      end
+
+      i += 1
+    end
+  end
+
+  new_array
 end
